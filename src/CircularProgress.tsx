9e5c498bdd26cd39a6d61d97e3302d2a21d24794
Doum <doum_art@hotmail.com>
2021-05-11 23:12:11 -0400

    chore: Add eslint and move react to peer dependencies

--- src/CircularProgress.tsx
+++ src/CircularProgress.tsx
@@ -1,22 +1,20 @@
-import GradientCircularProgress, { GradientCircularProgressProps } from "./GradientCircularProgress"
-import React from "react"
+import React from 'react';
+import GradientCircularProgress, { GradientCircularProgressProps } from './GradientCircularProgress';
 
-interface CircularProgressProps extends Omit<GradientCircularProgressProps, "startColor" | "middleColor" | "endColor"> {
+interface CircularProgressProps extends Omit<GradientCircularProgressProps, 'startColor' | 'middleColor' | 'endColor'> {
   color: string;
 }
 
 const CircularProgress: React.FunctionComponent<CircularProgressProps> = ({
   color,
   ...props
-}) => {
-  return (
+}) => (
     <GradientCircularProgress
       {...props}
       startColor={color}
       middleColor={color}
       endColor={color}
     />
-  )
-}
+  );
 
-export default CircularProgress
+export default CircularProgress;
